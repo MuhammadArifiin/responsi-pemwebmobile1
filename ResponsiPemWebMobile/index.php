0535874175fcc90d13cc82d7bf39a856c25df32f
ElsanPentanugraha <elsanpentanugraha687@gmail.com>
2021-05-23 15:41:23 +0700

    Responsi Praktikum
    Praktikum Pemrograman Web dan Mobile 1

---
+++ ResponsiPemWebMobile/index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>D'Corner Coffee | Home</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Damion' rel='stylesheet' type='text/css'>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/templatemo-style.css" rel="stylesheet">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+
+  </head>
+  <body>
+    <!-- Preloader -->
+    <div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
+    <!-- End Preloader -->
+    <div class="tm-top-header">
+      <div class="container">
+        <div class="row">
+          <div class="tm-top-header-inner">
+            <div class="tm-logo-container">
+              <img src="img/logo_dc.png" alt="Logo" class="tm-site-logo">
+              <h1 class="tm-site-name tm-handwriting-font">D'Corner Coffee</h1>
+            </div>
+            <div class="mobile-menu-icon">
+              <i class="fa fa-bars"></i>
+            </div>
+            <nav class="tm-nav">
+              <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="contact.php">Contact</a></li>
+              </ul>
+            </nav>   
+          </div>           
+        </div>    
+      </div>
+    </div>
+    <section class="tm-welcome-section">
+      <div class="container tm-position-relative">
+        <div class="tm-lights-container">
+          <img src="img/light.png" alt="Light" class="light light-1">
+          <img src="img/light.png" alt="Light" class="light light-2">
+          <img src="img/light.png" alt="Light" class="light light-3">  
+        </div>        
+        <div class="row tm-welcome-content">
+          <h2 class="white-text tm-handwriting-font tm-welcome-header"><img src="img/header-line.png" alt="Line" class="tm-header-line">&nbsp;Welcome To&nbsp;&nbsp;<img src="img/header-line.png" alt="Line" class="tm-header-line"></h2>
+          <h2 class="gold-text tm-welcome-header-2">D'CORNER COFFEE</h2>
+          <p class="gray-text tm-welcome-description"><?php 
+            echo "D'Corner Coffee adalah sebuah cafe yang menyediakan berbagai macam minuman kopi dan non-kopi yang berlokasi di Jalan Cakra Buana tepat di perempatan Jalan Agung dan berseberangan dengan gereja Bethani. D'Corner buka dari hari senin-jum'at mulai pukul 10.00-22.00 WIB dan untuk weekend mulai pukul 10.00-23.00 WIB.";
+          ?></p>
+          <a href="#main" class="tm-more-button tm-more-button-welcome">Details</a>      
+        </div>
+        <img src="img/table-set.png" alt="Table Set" class="tm-table-set img-responsive">             
+      </div>      
+    </section>
+    <div class="tm-main-section light-gray-bg">
+      <div class="container" id="main">      
+        <section class="tm-section tm-section-margin-bottom-0 row">
+          <div class="col-lg-12 tm-section-header-container">
+            <h2 class="tm-section-header gold-text tm-handwriting-font"><img src="img/logo.png" alt="Logo" class="tm-site-logo"> Popular Items</h2>
+            <div class="tm-hr-container"><hr class="tm-hr"></div>
+          </div>
+          <div class="col-lg-12 tm-popular-items-container">
+            <div class="tm-popular-item">
+              <img src="img/popular-menu-1.png" alt="Popular" class="tm-popular-item-img">
+              <div class="tm-popular-item-description">
+                <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">dc c</span>offee</h3><hr class="tm-popular-item-hr">
+                <p><?php 
+                  echo "DC Coffee adalah menu signature yang cocok dinikmati untuk nongkrong santuy bersama teman. <br><br><br>";
+                ?></p>
+                <div class="order-now-container">
+                  <a href="#" class="order-now-link tm-handwriting-font">Only </br> 18k</a>
+                </div>
+              </div>              
+            </div>
+            <div class="tm-popular-item">
+              <img src="img/popular-menu-2.png" alt="Popular" class="tm-popular-item-img">
+              <div class="tm-popular-item-description">
+                <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">b</span>lue lagoon</h3><hr class="tm-popular-item-hr">
+                <p><?php 
+                  echo "Blue Lagoon dengan campuran soda segar dan sirup manis yang memberikan sensasi kesegaran. <br><br><br>";
+                ?></p>
+                <div class="order-now-container">
+                  <a href="#" class="order-now-link tm-handwriting-font">Only </br> 18k</a>
+                </div>
+              </div>              
+            </div>
+            <div class="tm-popular-item">
+              <img src="img/popular-menu-3.png" alt="Popular" class="tm-popular-item-img">
+              <div class="tm-popular-item-description">
+                <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">m</span>ojito</h3><hr class="tm-popular-item-hr">
+                <p><?php 
+                  echo "Untuk kamu yang tidak terlalu menyukai kopi, Mojito merupakan pilihan yang tepat untuk move on dan menikmati rasa baru dari kesegaran lemon dan mint. <br>";
+                ?></p>
+                <div class="order-now-container">
+                  <a href="#" class="order-now-link tm-handwriting-font">Only </br> 18k</a>
+                </div>
+              </div>              
+            </div>
+          </div>          
+        </section>
+      </div>
+    </div> 
+    <footer>
+      <div class="tm-black-bg">
+        <div class="container">
+          <div class="row margin-bottom-60">
+            <nav class="col-lg-3 col-md-3 tm-footer-nav tm-footer-div">
+              <h3 class="tm-footer-div-title">Main Menu</h3>
+              <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Directory</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Our Services</a></li>
+              </ul>
+            </nav>
+            <div class="col-lg-5 col-md-5 tm-footer-div">
+              <h3 class="tm-footer-div-title">About Us</h3>
+              <p class="margin-top-15"> <?php 
+                echo "D'Corner Coffee adalah sebuah cafe yang menyediakan berbagai macam minuman kopi dan non-kopi. D'Corner Coffee berlokasi di Jalan Jl. Cakra Buana (Perempatan Jl.Agung), toko kami akhirnya bisa menjangkau pasar yang lebih luas lagi melalui internet.";
+              ?></p>
+              <p class="margin-top-15"><?php 
+                echo "D'Corner Coffee didirikan oleh Mas Andinu Wahyu Saputra pada Januari 2021. Dalam memberi layanan, kami selalu mencoba memberi persembahan terbaik kepada siapapun. Selain itu kami juga selalu menjunjung tinggi nilai-nilai etika yang baik, seperti kejujuran, ketepatan, dan profesionalitas dalam berbisnis.";
+              ?></p>
+              <p class="margin-top-15"><?php 
+                echo "A Good Coffee makes your day better";
+              ?></p>
+            </div>
+            <div class="col-lg-4 col-md-4 tm-footer-div">
+              <h3 class="tm-footer-div-title">Get Social</h3>
+              <p><?php 
+                echo "Ikuti Sosial Media kami untuk mendapatkan informasi tentang penawaran khusus produk kami.";
+              ?></p>
+              <div class="tm-social-icons-container">
+                <a href="https://www.instagram.com/dcornercoffeepky/" class="tm-social-icon"><i class="fa fa-instagram"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-youtube"></i></a>
+              </div>
+            </div>
+          </div>          
+        </div>  
+      </div>      
+      <div>
+        <div class="container">
+          <div class="row tm-copyright">
+           <p class="col-lg-12 small copyright-text text-center">Copyright &copy; 2021 D'Corner Coffee</p>
+         </div>  
+       </div>
+     </div>
+   </footer> <!-- Footer content-->  
+   <!-- JS -->
+   <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+   <script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+
+ </body>
+ </html>
---
+++ ResponsiPemWebMobile/menu_noncoffee.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>D'Corner Coffee | Menu</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Damion' rel='stylesheet' type='text/css'>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/templatemo-style.css" rel="stylesheet">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+
+  </head>
+  <body>
+    <!-- Preloader -->
+    <div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
+    <!-- End Preloader -->
+    <div class="tm-top-header">
+      <div class="container">
+        <div class="row">
+          <div class="tm-top-header-inner">
+            <div class="tm-logo-container">
+              <img src="img/logo_dc.png" alt="Logo" class="tm-site-logo">
+              <h1 class="tm-site-name tm-handwriting-font">D'Corner Coffee</h1>
+            </div>
+            <div class="mobile-menu-icon">
+              <i class="fa fa-bars"></i>
+            </div>
+            <nav class="tm-nav">
+              <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php" class="active">Menu</a></li>
+                <li><a href="contact.php">Contact</a></li>
+              </ul>
+            </nav>   
+          </div>           
+        </div>    
+      </div>
+    </div>
+    <section class="tm-welcome-section">
+      <div class="container tm-position-relative">
+        <div class="tm-lights-container">
+          <img src="img/light.png" alt="Light" class="light light-1">
+          <img src="img/light.png" alt="Light" class="light light-2">
+          <img src="img/light.png" alt="Light" class="light light-3">  
+        </div>        
+        <div class="row tm-welcome-content">
+          <h2 class="white-text tm-handwriting-font tm-welcome-header"><img src="img/header-line.png" alt="Line" class="tm-header-line">&nbsp;Our Menus&nbsp;&nbsp;<img src="img/header-line.png" alt="Line" class="tm-header-line"></h2>
+          <h2 class="gold-text tm-welcome-header-2">D'CORNER COFFEE</h2>
+          <p class="gray-text tm-welcome-description" style="text-align:center;"><?php
+            echo "D'Corner Coffee menyediakan beberapa pilihan minuman baik kopi maupun non-kopi.";
+          ?></p>
+          <a href="#main" class="tm-more-button tm-more-button-welcome">Check Our Menu</a>      
+        </div>
+        <img src="img/table-set.png" alt="Table Set" class="tm-table-set img-responsive">  
+      </div>      
+    </section>
+    <div class="tm-main-section light-gray-bg">
+      <div class="container" id="main">          
+        <section class="tm-section row">
+          <div class="col-lg-12 tm-section-header-container margin-bottom-30">
+            <h2 class="tm-section-header gold-text tm-handwriting-font"><img src="img/logo.png" alt="Logo" class="tm-site-logo"> Our Menus</h2>
+            <div class="tm-hr-container"><hr class="tm-hr"></div>
+          </div>
+          <div>
+            <div class="col-lg-3 col-md-3">
+              <div class="tm-position-relative margin-bottom-30">              
+                <nav class="tm-side-menu">
+                  <ul>
+                    <li><a href="menu.php">Coffee</a></li>
+                    <li><a href="menu_noncoffee.php" class="active">non-coffee</a></li>
+                  </ul>              
+                </nav>    
+                <img src="img/vertical-menu-button.png" alt="Menu bg" class="tm-side-menu-bg">
+              </div>  
+            </div>            
+            <div class="tm-menu-product-content col-lg-9 col-md-9"> <!-- menu content -->
+              <div class="tm-product">
+                <img src="img/green-tea.png" alt="Product">
+                <div class="tm-product-text">
+                  <h3 class="tm-product-title">Green Tea</h3>
+                </div>
+                <div class="tm-product-price">
+                  <a href="#" class="tm-product-price-link tm-handwriting-font"><span class="tm-product-price-currency"></span>16.5k</a>
+                </div>
+              </div>
+              <div class="tm-product">
+                <img src="img/taro.png" alt="Product">
+                <div class="tm-product-text">
+                  <h3 class="tm-product-title">Taro</h3>
+                </div>
+                <div class="tm-product-price">
+                  <a href="#" class="tm-product-price-link tm-handwriting-font"><span class="tm-product-price-currency"></span>16.5k</a>
+                </div>
+              </div>
+              <div class="tm-product">
+                <img src="img/red-velvet.png" alt="Product">
+                <div class="tm-product-text">
+                  <h3 class="tm-product-title">Red Velvet</h3>
+                </div>
+                <div class="tm-product-price">
+                  <a href="#" class="tm-product-price-link tm-handwriting-font"><span class="tm-product-price-currency"></span>16.5k</a>
+                </div>
+              </div>
+              <div class="tm-product">
+                <img src="img/coklat.png" alt="Product">
+                <div class="tm-product-text">
+                  <h3 class="tm-product-title">Coklat</h3>
+                </div>
+                <div class="tm-product-price">
+                  <a href="#" class="tm-product-price-link tm-handwriting-font"><span class="tm-product-price-currency"></span>16.5k</a>
+                </div>
+              </div>
+              <div class="tm-product">
+                <img src="img/blue-lagoon.png" alt="Product">
+                <div class="tm-product-text">
+                  <h3 class="tm-product-title">Blue Lagoon</h3>
+                </div>
+                <div class="tm-product-price">
+                  <a href="#" class="tm-product-price-link tm-handwriting-font"><span class="tm-product-price-currency"></span>18k</a>
+                </div>
+              </div>
+              <div class="tm-product">
+                <img src="img/mojito.png" alt="Product">
+                <div class="tm-product-text">
+                  <h3 class="tm-product-title">Mojito</h3>
+                </div>
+                <div class="tm-product-price">
+                  <a href="#" class="tm-product-price-link tm-handwriting-font"><span class="tm-product-price-currency"></span>18k</a>
+                </div>
+              </div>
+            </div>
+          </div>          
+        </section>
+      </div>
+    </div> 
+    <footer>
+      <div class="tm-black-bg">
+        <div class="container">
+          <div class="row margin-bottom-60">
+            <nav class="col-lg-3 col-md-3 tm-footer-nav tm-footer-div">
+              <h3 class="tm-footer-div-title">Main Menu</h3>
+              <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Directory</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Our Services</a></li>
+              </ul>
+            </nav>
+            <div class="col-lg-5 col-md-5 tm-footer-div">
+              <h3 class="tm-footer-div-title">About Us</h3>
+              <p class="margin-top-15"> <?php 
+                echo "D'Corner Coffee adalah sebuah cafe yang menyediakan berbagai macam minuman kopi dan non-kopi. D'Corner Coffee berlokasi di Jalan Jl. Cakra Buana (Perempatan Jl.Agung), toko kami akhirnya bisa menjangkau pasar yang lebih luas lagi melalui internet.";
+              ?></p>
+              <p class="margin-top-15"><?php 
+                echo "D'Corner Coffee didirikan oleh Mas Andinu Wahyu Saputra pada Januari 2021. Dalam memberi layanan, kami selalu mencoba memberi persembahan terbaik kepada siapapun. Selain itu kami juga selalu menjunjung tinggi nilai-nilai etika yang baik, seperti kejujuran, ketepatan, dan profesionalitas dalam berbisnis.";
+              ?></p>
+              <p class="margin-top-15"><?php 
+                echo "A Good Coffee makes your day better";
+              ?></p>
+            </div>
+            <div class="col-lg-4 col-md-4 tm-footer-div">
+              <h3 class="tm-footer-div-title">Get Social</h3>
+              <p><?php 
+                echo "Ikuti Sosial Media kami untuk mendapatkan informasi tentang penawaran khusus produk kami.";
+              ?></p>
+              <div class="tm-social-icons-container">
+                <a href="https://www.instagram.com/dcornercoffeepky/" class="tm-social-icon"><i class="fa fa-instagram"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-youtube"></i></a>
+              </div>
+            </div>
+          </div>          
+        </div>  
+      </div>      
+      <div>
+        <div class="container">
+          <div class="row tm-copyright">
+           <p class="col-lg-12 small copyright-text text-center">Copyright &copy; 2021 D'Corner Coffee</p>
+         </div>  
+       </div>
+     </div>
+   </footer> <!-- Footer content-->  
+   <!-- JS -->
+   <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+   <script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+
+ </body>
+ </html>
